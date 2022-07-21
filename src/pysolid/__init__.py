0eebd8dc0138910669c79f0cef70bf4e4d922fcb
Zhang Yunjun <yunjunzgeo@gmail.com>
2022-07-20 23:41:26 -0700

    __init__: explicit list of top-level functions

--- src/pysolid/__init__.py
+++ src/pysolid/__init__.py
@@ -1,8 +1,14 @@
 # get version info
-from pysolid.version import *
-__version__ = release_version
+from pysolid.version import release_version as __version__
 
 # top-level functions
-from pysolid.grid import *
-from pysolid.point import *
-
+from pysolid.grid import (
+    calc_solid_earth_tides_grid,
+    plot_solid_earth_tides_grid,
+)
+from pysolid.point import (
+    TIDES,
+    calc_solid_earth_tides_point,
+    plot_solid_earth_tides_point,
+    plot_power_spectral_density4tides,
+)
