c11e8ee7adb759eb83d40b215c47b8c9b3670650
Zhang Yunjun <yunjunzgeo@gmail.com>
2022-11-05 11:04:38 -0700

    add name to setup.py to avoid UNKNOWN name with setuptools<60

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ import setuptools
 from numpy.distutils.core import setup, Extension
 
 setup(
+    # avoid UNKNOWN name in setuptools<60, the latter is required for numpy.distutils
+    name='pysolid',
     # fortran extensions to build with numpy.f2py
     ext_modules=[
         Extension(name='pysolid.solid', sources=['src/pysolid/solid.for']),
