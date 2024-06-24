101e0a5366c765170828575966824fcdd47c7c13
Zhang Yunjun <yunjunz@outlook.com>
2024-06-24 15:45:12 +0800

    unit test: add numeric comparison (#90)
    + `tests.grid/point.py`: add numeric comparisons between the calculated results and the hardcore numpy array
    
    + `pyproject.toml`: attempt to fix [the `long_description` syntax error](https://github.com/insarlab/PySolid/actions/runs/9626458068/job/26552735067) while uploading to PyPI, without understanding the logic behind.

--- tests/point.py
+++ tests/point.py
@@ -7,6 +7,8 @@ import os
 import sys
 import datetime as dt
 
+import numpy as np
+
 import pysolid
 
 
@@ -18,14 +20,49 @@ if __name__ == '__main__':
 
     # prepare inputs
     lat, lon = 34.0, -118.0     # Los Angles, CA
-    dt_obj0 = dt.datetime(2020, 11,  1, 4, 0, 0)
-    dt_obj1 = dt.datetime(2020, 12, 31, 2, 0, 0)
+    dt_obj0 = dt.datetime(2020, 11,  5, 12, 0, 0)
+    dt_obj1 = dt.datetime(2020, 12, 31,  0, 0, 0)
+
+    # reference
+    # calculated based on version 0.3.2.post6 on Jun 24, 2024
+    # env: macOS with python-3.10, numpy-1.24
+    # install: manual compilation via f2py
+    dt_out_8000 = np.array(
+        [dt.datetime(2020, 11,  5, 12,  0),
+         dt.datetime(2020, 11, 11,  1, 20),
+         dt.datetime(2020, 11, 16, 14, 40),
+         dt.datetime(2020, 11, 22,  4,  0),
+         dt.datetime(2020, 11, 27, 17, 20),
+         dt.datetime(2020, 12,  3,  6, 40),
+         dt.datetime(2020, 12,  8, 20,  0),
+         dt.datetime(2020, 12, 14,  9, 20),
+         dt.datetime(2020, 12, 19, 22, 40),
+         dt.datetime(2020, 12, 25, 12,  0)], dtype=object,
+    )
+    tide_e_8000 = np.array(
+        [-0.02975027,  0.04146837, -0.02690945, -0.00019223,  0.01624152,
+          0.0532655 , -0.02140918, -0.05554432,  0.01371739, -0.00516968],
+    )
+    tide_n_8000 = np.array(
+        [-0.01275229, -0.02834036,  0.00886857, -0.03247227, -0.05237735,
+         -0.00590791, -0.01990448, -0.01964124, -0.04439581, -0.00410378],
+    )
+    tide_u_8000 = np.array(
+        [ 0.16008235, -0.05721991, -0.15654693, -0.00041214,  0.03041098,
+          0.13082217, -0.1006462 ,  0.24870719, -0.02648802, -0.08420228],
+    )
 
     # calculate
     (dt_out,
      tide_e,
      tide_n,
      tide_u) = pysolid.calc_solid_earth_tides_point(lat, lon, dt_obj0, dt_obj1, verbose=False)
+
+    # compare
+    assert all(dt_out[::8000] == dt_out_8000)
+    assert np.allclose(tide_e[::8000], tide_e_8000)
+    assert np.allclose(tide_n[::8000], tide_n_8000)
+    assert np.allclose(tide_u[::8000], tide_u_8000)
 
     # plot
     out_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'pic'))
